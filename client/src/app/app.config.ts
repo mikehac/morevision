6fa9917de87a6402404a27b257af0d746da70e48
Mike Hatskevich <mike.hatskevich@gmail.com>
2025-02-21 15:01:16 +0200

    Moving to Angular 19.1

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,13 +1,17 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
-import { FormsModule } from '@angular/forms';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 registerLocaleData(en);
 
@@ -15,8 +19,9 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    provideNzI18n(en_US),
+    provideZoneChangeDetection({ eventCoalescing: true }),
     importProvidersFrom(FormsModule),
     provideAnimationsAsync(),
+    provideNzI18n(en_US),
   ],
 };
